2d7c36af8db2a1af5fd01314aa934929baa3536d
Jinsung Oh <econquer@bucketplace.net>
2024-10-16 23:08:52 +0900

    Brew formula update for mortar-dev version 0.1.4

--- Formula/mortar-dev@0.rb
+++ Formula/mortar-dev@0.rb
@@ -1,11 +1,11 @@
 class MortarDevAT0 < Formula
   desc "Mortar - build and deploy your applications easy as eating a piece of cake"
   homepage "https://github.com/bucketplace/mortar"
-  version "0.1.3"
+  version "0.1.4"
   on_macos do
     on_arm do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar-dev/0.1.3/mortar-dev-darwin_arm64.tar.gz"
-      sha256 "4275ba759b747c14f5e04f572e4bd3babcbd5233629afbc44cb1adbf4aa63451"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar-dev/0.1.4/mortar-dev-darwin_arm64.tar.gz"
+      sha256 "a3c3d588baf25be12b2bf577dc78fa213dcdfffab2f1b75794ae5cb4602f3688"
       def install
         bin.install "mortar"
         prefix.install "version"
@@ -14,16 +14,16 @@ class MortarDevAT0 < Formula
   end
   on_linux do
     on_intel do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar-dev/0.1.3/mortar-dev-linux_amd64.tar.gz"
-      sha256 "919284b9d7e7c8b4f3b280fadcb828a5acfd38c3f5626058fd97f0959930a29b"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar-dev/0.1.4/mortar-dev-linux_amd64.tar.gz"
+      sha256 "898c8e0ec2ca1b7ec228af7f49b5b1314f7aa01c56299ce609e7cf2edb4ea42e"
       def install
         bin.install "mortar"
         prefix.install "version"
       end
     end
     on_arm do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar-dev/0.1.3/mortar-dev-linux_arm64.tar.gz"
-      sha256 "92b261ef8d181d4a171b9d44149089297f2b1f5f3df644543a603f196b6f2422"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar-dev/0.1.4/mortar-dev-linux_arm64.tar.gz"
+      sha256 "59b431bbfc95210a757d5d56b097e65b76a3606f84822c8e43efe3d6a9a0e68e"
       def install
         bin.install "mortar"
         prefix.install "version"
