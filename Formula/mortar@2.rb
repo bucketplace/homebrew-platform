cefcfa400d6e937e3e4f4c82e3617c3b2aa2b4c2
Wonjae <wonjae.kim@bucketplace.net>
2025-12-05 18:18:46 +0900

    Brew formula update for mortar version 2.6.3

--- Formula/mortar@2.rb
+++ Formula/mortar@2.rb
@@ -1,11 +1,11 @@
 class MortarAT2 < Formula
   desc "Mortar - build and deploy your applications easy as eating a piece of cake"
   homepage "https://github.com/bucketplace/mortar"
-  version "2.6.2"
+  version "2.6.3"
   on_macos do
     on_arm do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.6.2/mortar_darwin_arm64.tar.gz"
-      sha256 "efc30a1f4d09b725dd4782369bb4298a505d3e747855b47dd9e591cbf1e56f78"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.6.3/mortar_darwin_arm64.tar.gz"
+      sha256 "5d8a5f6184db22c4840a18353f8c3b02d11dbb27f5211de0de8eee4ed4b87154"
       def install
         bin.install "mortar"
         prefix.install "version"
@@ -14,16 +14,16 @@ class MortarAT2 < Formula
   end
   on_linux do
     on_intel do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.6.2/mortar_linux_amd64.tar.gz"
-      sha256 "d1e5fdf91e5187d1daf6ab6734be9cbe433fe498fb5440ca61cc1488c9883913"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.6.3/mortar_linux_amd64.tar.gz"
+      sha256 "719b6c0dcbc9d9bcf62ea252f87d6573540cf1bd4b1c1bbe0875dd47e708ec0d"
       def install
         bin.install "mortar"
         prefix.install "version"
       end
     end
     on_arm do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.6.2/mortar_linux_arm64.tar.gz"
-      sha256 "c9463fe30856fe7f7f86f0a1205ae788b25713358fecc6ebe4ec95c5c548ab85"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/platform/mortar/2.6.3/mortar_linux_arm64.tar.gz"
+      sha256 "6bce9d8ed4d2614e9b2391a50f38546571c18335f295a5c38bb99293f4d7723d"
       def install
         bin.install "mortar"
         prefix.install "version"
