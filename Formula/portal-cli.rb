e4b7991176848121d256da15bca0a83c18a2c328
??? <51079303+bp-deployer@users.noreply.github.com>
2026-04-10 16:44:45 +0900

    Update portal-cli formulas to 0.1.3
    File list:
    - Formula/portal-cli.rb
    - Formula/portal-cli@0.rb
    - Formula/portal-cli@0.1.rb

--- Formula/portal-cli.rb
+++ Formula/portal-cli.rb
@@ -6,7 +6,7 @@ class PortalCli < Formula
   on_macos do
     on_arm do
       url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.3/portal-cli_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "1e99e2562acfb5a1b0a5cae05769a0c08887d82e6bd924c4e9e014233895e664"
+      sha256 "c89c55219c45fbd9007c8f3ea5f17bdb8ab53aa7689671570511dd235344c5e7"
 
       def install
         bin.install "portal"
@@ -14,7 +14,7 @@ class PortalCli < Formula
     end
     on_intel do
       url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.3/portal-cli_darwin_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "f389cd6c7c2bb37768cf4f14ccae3cbd7a46f2bb499159865ba452c38a136d11"
+      sha256 "bd9d5b24290eaf50101230ac55f5cc456f782e061480a8dde411b2872b235281"
 
       def install
         bin.install "portal"
@@ -25,7 +25,7 @@ class PortalCli < Formula
   on_linux do
     on_intel do
       url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.3/portal-cli_linux_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "923a289ced1152184742843085c489b8cdf9f7c1b24d99b675dacd8186afc184"
+      sha256 "3ef076d438bb87bfe7a3a1e03795cf941025b10072fbf9d5f57c959c2aa9ebf2"
 
       def install
         bin.install "portal"
@@ -33,7 +33,7 @@ class PortalCli < Formula
     end
     on_arm do
       url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.3/portal-cli_linux_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "99f9789ec7162f25890681a7cb66f2487d662c85d05d65c89b75f764fccc0f50"
+      sha256 "b0fa4409044ad586116b9b34cfd7bbd6619b99f1cbf1c45e3b6f8a0a0ad4ab2f"
 
       def install
         bin.install "portal"
