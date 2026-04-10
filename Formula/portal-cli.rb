768fb725a82800cf1b75de29f6fe5890253f97e0
??? <51079303+bp-deployer@users.noreply.github.com>
2026-04-10 16:20:45 +0900

    Update portal-cli formulas to 0.1.3
    File list:
    - Formula/portal-cli.rb
    - Formula/portal-cli@0.rb
    - Formula/portal-cli@0.1.rb

--- Formula/portal-cli.rb
+++ Formula/portal-cli.rb
@@ -1,12 +1,12 @@
 class PortalCli < Formula
   desc "Portal CLI - dev-portal command-line tool for humans and AI agents"
   homepage "https://github.com/bucketplace"
-  version "0.1.3-10"
+  version "0.1.3"
 
   on_macos do
     on_arm do
       url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.3/portal-cli_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "cafb5a3e702fd638009857419e75f4d874df0e203f84feef8ce9d4b57c32fa28"
+      sha256 "1e99e2562acfb5a1b0a5cae05769a0c08887d82e6bd924c4e9e014233895e664"
 
       def install
         bin.install "portal"
@@ -14,7 +14,7 @@ class PortalCli < Formula
     end
     on_intel do
       url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.3/portal-cli_darwin_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "596a9b68687e4d28a1f136c2faf84b595b9767ecf9eb5a45315867120af5e0b1"
+      sha256 "f389cd6c7c2bb37768cf4f14ccae3cbd7a46f2bb499159865ba452c38a136d11"
 
       def install
         bin.install "portal"
@@ -25,7 +25,7 @@ class PortalCli < Formula
   on_linux do
     on_intel do
       url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.3/portal-cli_linux_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "d3ed5dd8e56921a57f0b5deed4db10e2bcd81d534916d9e93919589519448c4d"
+      sha256 "923a289ced1152184742843085c489b8cdf9f7c1b24d99b675dacd8186afc184"
 
       def install
         bin.install "portal"
@@ -33,7 +33,7 @@ class PortalCli < Formula
     end
     on_arm do
       url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.3/portal-cli_linux_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "654189287c9b63731fd7597ba561e6e97dce15d29bc6ef2e7945c7d75ac831eb"
+      sha256 "99f9789ec7162f25890681a7cb66f2487d662c85d05d65c89b75f764fccc0f50"
 
       def install
         bin.install "portal"
