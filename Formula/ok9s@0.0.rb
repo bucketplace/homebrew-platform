6730f75f5cb028eb0aee8d554d10cf0265ab232e
????????? <51079303+bp-deployer@users.noreply.github.com>
2026-07-08 18:55:08 +0900

    Update ok9s formulas to 0.0.1
    File list:
    - Formula/ok9s.rb
    - Formula/ok9s@0.rb
    - Formula/ok9s@0.0.rb

--- Formula/ok9s@0.0.rb
+++ Formula/ok9s@0.0.rb
@@ -1,21 +1,21 @@
 class Ok9sAT00 < Formula
   desc "k9s helper for ohouse member"
   homepage "https://github.com/bucketplace"
-  version "0.0.0"
+  version "0.0.1"
   depends_on "bucketplace/platform/portal-cli"
 
   on_macos do
     on_arm do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/ok9s/0.0.0/ok9s_0.0.0_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "d436c77a7f7d4038f80f18ca2bd2f2de9e2d23e43171a4999d1b09730b421d56"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/ok9s/0.0.1/ok9s_0.0.0_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "6cb1edfbea7d84924c81ddc9c7dc1f84ffa4270551b6627cdc005100421e94cb"
 
       def install
         bin.install "ok9s"
       end
     end
     on_intel do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/ok9s/0.0.0/ok9s_0.0.0_darwin_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "4c3a2838084d8d6215bb7d1e3022bd111dca508b15e64785880f681077da2ec0"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/ok9s/0.0.1/ok9s_0.0.0_darwin_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "09dade7715161e95dc660827ee3998e518f16d945e0db4b307b86b871132d27e"
 
       def install
         bin.install "ok9s"
@@ -25,16 +25,16 @@ class Ok9sAT00 < Formula
 
   on_linux do
     on_intel do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/ok9s/0.0.0/ok9s_0.0.0_linux_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "7d4420f8279dde2a21f4bc9c98fd2218655f9b59ece22a01e616a2e3a8aa529f"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/ok9s/0.0.1/ok9s_0.0.0_linux_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "dafb3b8268cbfd29f7f15997b4bea75f1d7d076db0aa3f037c6f7c10ee3f0b9f"
 
       def install
         bin.install "ok9s"
       end
     end
     on_arm do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/ok9s/0.0.0/ok9s_0.0.0_linux_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "4699269a61b9fa5239ff4a57d3ed32893aa34253840609f0c26d1fab625b0153"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/ok9s/0.0.1/ok9s_0.0.0_linux_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "b9073647906e0449113a3d5639295663f448f313f5071aac5bfe659fd14b11ff"
 
       def install
         bin.install "ok9s"
