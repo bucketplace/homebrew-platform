ba1c81cc1efc52ba04e328493e310c13336b506f
????????? <51079303+bp-deployer@users.noreply.github.com>
2026-08-06 14:33:28 +0900

    Update portal-cli formulas to 0.1.19-26
    File list:
    - Formula/portal-cli-nightly.rb
    - Formula/portal-cli-nightly@0.1.19.rb

--- Formula/portal-cli-nightly.rb
+++ Formula/portal-cli-nightly.rb
@@ -1,20 +1,20 @@
 class PortalCliNightly < Formula
   desc "Portal CLI - dev-portal command-line tool for humans and AI agents"
   homepage "https://github.com/bucketplace"
-  version "0.1.19-25"
+  version "0.1.19-26"
 
   on_macos do
     on_arm do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.19-25/portal-cli-nightly_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "525e44015c80d62f5556a3f87af528b425f301c9e0b9e2e151cabc3135f0f801"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.19-26/portal-cli-nightly_darwin_arm64.tar.gz", using: CurlDownloadStrategy
+      sha256 "4cda68afcc77f5993f4d75a01321f0814fd58ef4e6a542d848752c6c4370d912"
 
       def install
         bin.install "portal"
       end
     end
     on_intel do
-      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.19-25/portal-cli-nightly_darwin_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "e58728f7d39e2643fcfce6d2ae27aec1cf4072f2bcf2476a59bbbe8cd97a4bfc"
+      url "https://nexus.co-workerhou.se/repository/raw-tool-releases/homebrew/cli/portal-cli/0.1.19-26/portal-cli-nightly_darwin_amd64.tar.gz", using: CurlDownloadStrategy
+      sha256 "51cc96c81ee59774d254199079b0156005ceaafc04c3154ce718a2bc1f031120"
 
       def install
         bin.install "portal"
